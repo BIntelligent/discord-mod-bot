4d5bb91a6b9a300f92847fc0179b94165b63bccc
BIntelligent <65684589+BIntelligent@users.noreply.github.com>
2021-06-29 08:28:56 -0700

    Mongoodb OWowowow

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const { Client } = require('discord.js')
+const {
+    Client
+} = require('discord.js')
 
-module.exports = async(client) => {
+module.exports = async (client, url) => {
 
     const ClientCheck = client instanceof Client
     if (!client || ClientCheck === false) throw new Error("INVALID_CLIENT: There is no discord.js Client provided or it is invalid..")
+    if (!url || typeof url !== "string" || !url.startsWith("mongodb://") || !url.startsWith("mongodb+srv")) throw new Error("INVALID_URL: There is no Mongodb URl provided or it is invalid..")
 
     console.log(`Discord-Mod-Bot Package succesfully started up.. And works!`)
 
-    module.exports.client = client
+    module.exports.client = client;
+
+    require('mongodb').MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        client.db = db.db("moderation");
+    });
 
 }
 
